19051dff41d2ce4b1944ce29252eb881d0d5f628
Ghislain Rodrigues <git@ghislain-rodrigues.fr>
2016-06-30 15:16:22 +0100

    argument added to toggle the fullscreen flag

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,27 @@
 #include <SDL2/SDL.h>
+#include <unistd.h>
 #include "Game.h"
+
+void parseArguments(int argc, char* args[], bool *fullScreen) {
+	int c;
+	while ((c = getopt (argc, args, "f")) != -1) {
+		switch (c) {
+			case 'f':
+				*fullScreen = true;
+				break;
+			default:
+				abort();
+		}
+	}
+}
 
 int main(int argc, char* args[]) {
 	Game* g = new Game();
+	bool fullScreen;
 
-	if (!g->init("My first window", 100, 100, 640, 480, true)) {
+	parseArguments(argc, args, &fullScreen);
+
+	if (!g->init("My first window", 100, 100, 640, 480, fullScreen)) {
 		return 1;
 	}
 
