eb96b434558670ac799d9ba9ec78d353ebce560c
Ghislain Rodrigues <git@ghislain-rodrigues.fr>
2016-07-11 09:17:05 +0100

    those signatures are not needed anymore

--- src/GameObject.h
+++ src/GameObject.h
@@ -7,11 +7,7 @@
 class GameObject {
 	public:
 	virtual ~GameObject() {}
-	virtual void load(const int x, const int y, const int width, const int height) = 0;
-	virtual void setTexture(const std::string textureID, int nbFrames) = 0;
-	virtual void setTextureRow(const int currentRow) = 0;
 	virtual void update() = 0;
-	virtual void render(SDL_Renderer* pRenderer) = 0;
 };
 
 #endif
