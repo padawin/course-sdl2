f1388fa8d78379c164642f3289727b4b181795fd
Ghislain Rodrigues <git@ghislain-rodrigues.fr>
2016-07-14 22:33:02 +0100

    Methods added to manipulate joysticks

--- src/InputHandler.cpp
+++ src/InputHandler.cpp
@@ -1,4 +1,6 @@
 #include "InputHandler.h"
+#include <iostream>
+#include <SDL2/SDL.h>
 
 static InputHandler* s_pInstance;
 
@@ -16,6 +18,44 @@ bool InputHandler::update() {
 	bool ret = true;
 
 	return ret;
+}
+
+void InputHandler::initialiseJoysticks() {
+	if (SDL_WasInit(SDL_INIT_JOYSTICK) == 0) {
+		SDL_InitSubSystem(SDL_INIT_JOYSTICK);
+	}
+
+	if (SDL_NumJoysticks() == 0) {
+		m_bJoysticksInitialised = false;
+	}
+	else {
+		for (int i = 0; i < SDL_NumJoysticks(); i++) {
+			SDL_Joystick* joy = SDL_JoystickOpen(i);
+			if (joy) {
+				m_joysticks.push_back(joy);
+			}
+			else {
+				std::cout << SDL_GetError();
+			}
+		}
+		SDL_JoystickEventState(SDL_ENABLE);
+		m_bJoysticksInitialised = true;
+		std::cout << "Initialised "<< m_joysticks.size() << " joystick(s)\n";
+	}
+}
+
+void InputHandler::clean() {
+	if (m_bJoysticksInitialised) {
+		for (int i = 0; i < SDL_NumJoysticks(); i++){
+			SDL_JoystickClose(m_joysticks[i]);
+		}
+
+		std::cout << "Cleaned "<< m_joysticks.size() << " joystick(s)\n";
+	}
+}
+
+bool InputHandler::joysticksInitialised() {
+	return m_bJoysticksInitialised;
 }
 
 void InputHandler::free() {
--- src/InputHandler.h
+++ src/InputHandler.h
@@ -1,14 +1,22 @@
 #ifndef __InputHandler__
 #define __InputHandler__
 
+#include <SDL2/SDL_joystick.h>
+#include <vector>
+
 class InputHandler {
 	private:
 	InputHandler();
+	std::vector<SDL_Joystick*> m_joysticks;
+	bool m_bJoysticksInitialised;
 
 	public:
 	static InputHandler* Instance();
 	static void free();
 	bool update();
+	void clean();
+	void initialiseJoysticks();
+	bool joysticksInitialised();
 };
 
 #endif
