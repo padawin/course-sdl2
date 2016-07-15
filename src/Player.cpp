2e61caf47e931821628bec9dd845008bee9cb018
Ghislain Rodrigues <git@ghislain-rodrigues.fr>
2016-07-15 15:36:11 +0100

    speed stored in a variable

--- src/Player.cpp
+++ src/Player.cpp
@@ -3,16 +3,16 @@
 #include <iostream>
 
 void Player::handleInput() {
-	int xAxisValue, yAxisValue;
+	int xAxisValue, yAxisValue, velocityBasis = 1;
 	InputHandler* handlerInstance = InputHandler::Instance();
 	if (handlerInstance->joysticksInitialised()) {
 		xAxisValue = handlerInstance->stickXValue(0, LEFT_STICK);
 		yAxisValue = handlerInstance->stickYValue(0, LEFT_STICK);
 		if (xAxisValue > 0 || xAxisValue < 0) {
-			m_velocity.setX(1 * xAxisValue);
+			m_velocity.setX(velocityBasis * xAxisValue);
 		}
 		if (yAxisValue > 0 || yAxisValue < 0) {
-			m_velocity.setY(1 * yAxisValue);
+			m_velocity.setY(velocityBasis * yAxisValue);
 		}
 	}
 }
