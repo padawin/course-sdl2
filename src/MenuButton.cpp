517e9d38b93427f676cd7359341fadc3acaf498b
Ghislain Rodrigues <git@ghislain-rodrigues.fr>
2016-07-18 12:21:38 +0100

    Actions plugged on the menu

--- src/MenuButton.cpp
+++ src/MenuButton.cpp
@@ -4,3 +4,11 @@ void MenuButton::setActive(const bool active) {
 	m_pActive = active;
 	m_iCurrentFrame = (int) active;
 }
+
+void MenuButton::setAction(void (*callback)()) {
+	m_callback = callback;
+}
+
+void MenuButton::executeAction() {
+	m_callback();
+}
