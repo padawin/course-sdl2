5b2688acbfaad28be82f586ce24eb56c404fbd84
Ghislain Rodrigues <git@ghislain-rodrigues.fr>
2016-07-20 19:09:00 +0100

    player comments added

--- src/Player.cpp
+++ src/Player.cpp
@@ -1,6 +1,12 @@
 #include "Player.h"
 #include "InputHandler.h"
 
+/**
+ * The joystick button and sticks states are checked. If the button 1 (B on
+ * xbox controller) is pressed, the player's speed is increased (it makes him
+ * run), if the left stick is tilted in any direction, the player's velocity is
+ * set.
+ */
 void Player::handleInput() {
 	float xAxisValue, yAxisValue, velocityBasis = 1.0;
 	InputHandler* handlerInstance = InputHandler::Instance();
@@ -21,6 +27,9 @@ void Player::handleInput() {
 	}
 }
 
+/**
+ * Reset the player velocity and update the input even polling and the player
+ */
 void Player::update() {
 	m_velocity.setX(0);
 	m_velocity.setY(0);
