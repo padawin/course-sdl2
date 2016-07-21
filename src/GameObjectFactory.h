05acec92f48b3fe13c9006cb37bb6c9debcdbcba
Ghislain Rodrigues <git@ghislain-rodrigues.fr>
2016-07-21 12:25:04 +0100

    GameObjectFactory and GameObjectCreator classes added
    missing methods added

---
+++ src/GameObjectFactory.h
@@ -0,0 +1,44 @@
+#ifndef __GameObjectFactory__
+#define __GameObjectFactory__
+
+#include <string>
+#include <map>
+#include "GameObject.h"
+#include "GameObjectCreator.h"
+
+class GameObjectFactory {
+	private:
+	/**
+	 * The game object factory is a singleton, so its constructor and
+	 * destructor are private
+	 */
+	GameObjectFactory();
+	~GameObjectFactory();
+
+	std::map<std::string, GameObjectCreator*> m_creators;
+
+	public:
+	/**
+	 * Because of the singleton, the following construct and operator are
+	 * removed.
+	 */
+	GameObjectFactory(GameObjectFactory const&) = delete;
+	void operator=(GameObjectFactory const&) = delete;
+
+	/**
+	 * Method to get the game object factory instance.
+	 */
+	static GameObjectFactory* Instance();
+
+	/**
+	 * Method to register a game object type
+	 */
+	bool registerType(std::string typeID, GameObjectCreator* pCreator);
+
+	/**
+	 * Method to instanciate a game object based on its type id
+	 */
+	GameObject* create(std::string typeID);
+};
+
+#endif
