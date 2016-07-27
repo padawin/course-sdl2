80af85a80c170066583924934b1e836d72e20e60
Ghislain Rodrigues <git@ghislain-rodrigues.fr>
2016-07-27 23:13:12 +0100

    every layers of a level updated and rendered

--- src/Level.cpp
+++ src/Level.cpp
@@ -7,3 +7,15 @@ std::vector<Tileset>* Level::getTilesets() {
 std::vector<Layer*>* Level::getLayers() {
 	return &m_vLayers;
 }
+
+void Level::render() {
+	for (int i = 0; i < m_vLayers.size(); i++) {
+		m_vLayers[i]->render();
+	}
+}
+
+void Level::update() {
+	for (int i = 0; i < m_vLayers.size(); i++) {
+		m_vLayers[i]->update();
+	}
+}
