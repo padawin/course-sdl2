2cf40a6dd029bbedb6803d36cf143e741b748041
Ghislain Rodrigues <git@ghislain-rodrigues.fr>
2017-02-15 08:03:41 +0000

    guard ifndef added for base64

--- src/base64.h
+++ src/base64.h
@@ -1,5 +1,9 @@
+#ifndef __BASE64__
+#define __BASE64__
+
 #include <string>
 
 std::string base64_encode(unsigned char const* , unsigned int len);
 std::string base64_decode(std::string const& s);
 
+#endif
